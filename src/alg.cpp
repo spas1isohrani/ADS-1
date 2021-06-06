97c1f8761a582497752b7f5d0f78dd7bfe73b66b
spas1isohrani <78902102+spas1isohrani@users.noreply.github.com>
2021-06-06 14:46:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+int cbinsearch(int *arr, int SIZE, int value) {
+  int a;
+  int count = 0, leftborder = 0, rightborder = SIZE;
+  
+   do {
+      int i = (leftborder + rightborder);
+      a = i;
+      if (arr[i] == value) {
+        count++;
+        while (arr[i] == value) {
+          i--;
+          if (arr[i] == value) {
+            count++;
+          }
+        }
+        while (arr[a] == value) {
+          a++;
+          if (arr[a] == value) {
+            count++;
+          }
+        }
+        return count;
+      } else if (arr[i] > value) {
+        rightborder = i - 1;
+      }
+     while (leftborder <= rightborder);
   return 0; // если ничего не найдено
 }
